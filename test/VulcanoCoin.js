36fb50de7a1a6e81143c1d98f71b82bb4ede792b
Anthony <anthonycolas.france@gmail.com>
2022-10-29 18:40:06 +0200

    test: Helper installed

--- test/VulcanoCoin.js
+++ test/VulcanoCoin.js
@@ -1,20 +1,27 @@
 const { expect } = require('chai');
 const hre = require('hardhat');
 const { ethers } = require('hardhat');
+const { loadFixture } = require('@nomicfoundation/hardhat-network-helpers');
 
 describe('VulcanoCoin Smart Contract', function () {
-  it('Should check the initial total supply', async function () {
-    const [owner] = await ethers.getSigners();
+  async function deployTokenFixture() {
     const VulcanoCoin = await ethers.getContractFactory('VulcanoCoin');
+    const [owner] = await ethers.getSigners();
     const vulcanoCoin = await VulcanoCoin.deploy();
+    await vulcanoCoin.deployed();
+
+    // Fixtures can return anything you consider useful for your tests
+    return { VulcanoCoin, vulcanoCoin, owner };
+  }
+
+  it('Should check the initial total supply', async function () {
+    const { vulcanoCoin, owner } = await loadFixture(deployTokenFixture);
 
     const ownerBalance = await vulcanoCoin.getUserBalance(owner.address);
     expect(await vulcanoCoin.getTotalSupply()).to.equal(ownerBalance);
   });
   it('Should increase the total supply by 1000', async function () {
-    const [owner] = await ethers.getSigners();
-    const VulcanoCoin = await ethers.getContractFactory('VulcanoCoin');
-    const vulcanoCoin = await VulcanoCoin.deploy();
+    const { vulcanoCoin, owner } = await loadFixture(deployTokenFixture);
 
     await vulcanoCoin.increaseTotalSupply();
     expect(await vulcanoCoin.getTotalSupply()).to.equal(11000);
