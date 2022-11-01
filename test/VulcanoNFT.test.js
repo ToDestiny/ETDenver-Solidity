591994a553f9a5ec495ac36b8c0e8033af12770a
Anthony <anthonycolas.france@gmail.com>
2022-11-01 19:08:54 +0100

    VolcanoNFT

---
+++ test/VulcanoNFT.test.js
@@ -0,0 +1,33 @@
+const { expect } = require('chai');
+const hre = require('hardhat');
+const { ethers } = require('hardhat');
+const { loadFixture } = require('@nomicfoundation/hardhat-network-helpers');
+
+describe('VolcanoNFT Smart Contract', function () {
+  async function deployTokenFixture() {
+    const VolcanoNFT = await ethers.getContractFactory('VolcanoNFT');
+    const [owner, addr1] = await ethers.getSigners();
+    const volcanoNFT = await VolcanoNFT.deploy();
+    await volcanoNFT.deployed();
+
+    // Fixtures can return anything you consider useful for your tests
+    return { VolcanoNFT, volcanoNFT, owner, addr1 };
+  }
+
+  it('Should check the minting function', async function () {
+    const { volcanoNFT, owner } = await loadFixture(deployTokenFixture);
+
+    await volcanoNFT.mintVolcanoNFT(owner.address);
+    expect(await volcanoNFT.balanceOf(owner.address)).to.equal('1');
+  });
+
+  it('Should check the transfer function', async function () {
+    const { volcanoNFT, owner, addr1 } = await loadFixture(deployTokenFixture);
+
+    await volcanoNFT.mintVolcanoNFT(owner.address);
+    const basicTransferTx = await volcanoNFT[
+      'safeTransferFrom(address, address, uint256)'
+    ](owner.address, addr1.address, 0);
+    expect(await volcanoNFT.balanceOf(addr1)).to.equal('1');
+  });
+});
