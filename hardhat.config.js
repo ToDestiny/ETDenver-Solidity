13d76231cdd7458d44b9bba458ba4119ce7dd31c
Anthony <anthonycolas.france@gmail.com>
2022-11-07 15:56:16 +0100

    Gas optimisation

--- hardhat.config.js
+++ hardhat.config.js
@@ -2,5 +2,13 @@ require('@nomiclabs/hardhat-waffle');
 
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
-  solidity: '0.8.9',
+  solidity: {
+    version: '0.8.9',
+    settings: {
+      optimizer: {
+        enabled: true,
+        runs: 1000,
+      },
+    },
+  },
 };
