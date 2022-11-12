37e80f41b0d5eca66f0cf775d5946f1e01c692b8
Anthony <anthonycolas.france@gmail.com>
2022-11-12 21:43:48 +0100

    D15

--- contracts/ethdenver_lessons/D16/VolcanoNFT.sol
+++ contracts/ethdenver_lessons/D16/VolcanoNFT.sol
@@ -1,23 +1,40 @@
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.8.9;
+pragma solidity ^0.8.17;
 
-import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
-import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 import "@openzeppelin/contracts/utils/Counters.sol";
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
+import "@openzeppelin/contracts/utils/Strings.sol";
+
+import "./VolcanoCoin.sol";
 
 contract VolcanoNFT is ERC721, ERC721URIStorage, Ownable {
     using Counters for Counters.Counter;
-
     Counters.Counter private _tokenIdCounter;
 
-    constructor() ERC721("VolcanoNFT", "VCN") {}
+    uint256 constant ETH_MINT_PRICE = 0.001 ether;
+    uint256 constant VOLCANOCOIN_MINT_PRICE = 1 ether; // Not actual ether, just used to multiply by 10^decimals()
 
-    function safeMint(address to, string memory uri) public onlyOwner {
-        uint256 tokenId = _tokenIdCounter.current();
+    VolcanoCoin private immutable volcanoCoin;
+
+    constructor(VolcanoCoin _volcanoCoin) ERC721("VolcanoNFT", "VCN") {
+        volcanoCoin = _volcanoCoin;
+    }
+
+    function mint() public payable returns(uint256) {
+        if (msg.value < ETH_MINT_PRICE) {
+            require(
+                volcanoCoin.transferFrom(msg.sender, address(this), VOLCANOCOIN_MINT_PRICE),
+                "Mint price is either 0.001 ETH or 1 VCN."
+            );
+        }
+
         _tokenIdCounter.increment();
-        _safeMint(to, tokenId);
-        _setTokenURI(tokenId, uri);
+        uint256 tokenId = _tokenIdCounter.current();
+        _safeMint(msg.sender, tokenId);
+
+        return tokenId;
     }
 
     // The following functions are overrides required by Solidity.
@@ -35,8 +52,8 @@ contract VolcanoNFT is ERC721, ERC721URIStorage, Ownable {
         return super.tokenURI(tokenId);
     }
 
-        function withdraw(address payable recipient) public onlyOwner {
-        uint256 balance = address(this).balance;
-        recipient.transfer(balance);
+    function withdraw() public onlyOwner returns (bool success) {
+        (success,) = payable(msg.sender).call{value: address(this).balance}("");
+        require(success, "Withdraw failed");
     }
 }
